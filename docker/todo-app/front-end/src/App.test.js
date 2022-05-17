6d5101a39cb70c66d20bd51710270cacc0e9ff4b
Maju Domenciano <juliadomenciano@hotmail.com>
2022-05-17 20:05:12 -0300

    commit para testar o avaliador

--- docker/todo-app/front-end/src/App.test.js
+++ docker/todo-app/front-end/src/App.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  console.log('oi')
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
